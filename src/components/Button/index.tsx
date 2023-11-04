9cf0a6c1c246b3e75ae1e1e879f736fc1763a3f6
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-11-04 20:25:27 -0300

    :sparkles:	feat: component button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,25 @@
+interface ButtonProps {
+  handleActionUser: () => void
+  value: string | number
+  buttonType?: 'button' | 'submit' | 'reset' | undefined
+}
+
+export function Button({
+  handleActionUser,
+  value,
+  buttonType,
+  ...rest
+}: ButtonProps) {
+  return (
+    <>
+      <button
+        onClick={handleActionUser}
+        type={buttonType}
+        className="bg-emerald-500 p-2 text-white font-semibold rounded"
+        {...rest}
+      >
+        {value}
+      </button>
+    </>
+  )
+}
