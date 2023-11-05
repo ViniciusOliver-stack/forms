3d842c7db6c8d4cca21b8fc4b8df2fe7d800056b
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-11-05 19:38:19 -0300

    feat: info example

---
+++ src/utils/quest.ts
@@ -0,0 +1,14 @@
+export const quest = [
+  {
+    id: 1,
+    quest: 'A qualidade do atendimento',
+  },
+  {
+    id: 2,
+    quest: 'A qualidade do atendimento 1',
+  },
+  {
+    id: 3,
+    quest: 'A qualidade do atendimento 2',
+  },
+]
